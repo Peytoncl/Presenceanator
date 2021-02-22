e3460874f7d2501ffa140483986164d57195f0eb
Peytoncl <peytonthegreat0@gmail.com>
2021-02-22 15:03:12 -0500

    init

---
+++ Files/src/main.js
@@ -0,0 +1,21 @@
+const DiscordRPC = require('discord-rpc');
+startButton = document.getElementById('start_button');
+clientId = document.getElementById('client_input').value;
+details = document.getElementById('detail_input').value;
+startButton.onclick = start;
+
+function start() {
+  clientId = document.getElementById('client_input').value;
+  details = document.getElementById('detail_input').value;
+
+    const client = new DiscordRPC.Client({ transport: 'ipc' });
+
+    client.on('ready', () => {
+        client.setActivity({
+            largeImageKey: 'test',
+            details: details
+        });
+    });
+  
+    client.login({ clientId: clientId });
+}
